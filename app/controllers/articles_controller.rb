8237cc0e6f565af1c08d531ed5c37ba504d1d639
Kokai Takahito <vistlip1192@gmail.com>
2025-09-08 18:21:52 +0900

    fromを部分テンプレートに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -14,23 +15,21 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article), notice: '保存できたよ'
+      redirect_to article_path(@article), notice: "\u4FDD\u5B58\u3067\u304D\u305F\u3088"
     else
-      flash.now[:error] = '保存に失敗しました'
+      flash.now[:error] = "\u4FDD\u5B58\u306B\u5931\u6557\u3057\u307E\u3057\u305F"
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-      redirect_to article_path(@article), notice: '保存できました'
+      redirect_to article_path(@article), notice: "\u4FDD\u5B58\u3067\u304D\u307E\u3057\u305F"
     else
-      flash.now[:error] = '更新できませんでした'
+      flash.now[:error] = "\u66F4\u65B0\u3067\u304D\u307E\u305B\u3093\u3067\u3057\u305F"
       render :edit, status: :unprocessable_entity
     end
   end
@@ -38,7 +37,7 @@ class ArticlesController < ApplicationController
   def destroy
     article = Article.find(params[:id])
     article.destroy!
-    redirect_to root_path, status: :see_other, notice: '削除に成功しました'
+    redirect_to root_path, status: :see_other, notice: "\u524A\u9664\u306B\u6210\u529F\u3057\u307E\u3057\u305F"
   end
 
   private
@@ -46,4 +45,7 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :content)
   end
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
